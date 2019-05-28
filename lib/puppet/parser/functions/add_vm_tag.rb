dcaf809d73ae8d5233e4e942a52946b922fdfc7a
krebsst <stefan.krebs@srf.ch>
2019-05-28 13:55:43 +0200

    only add tag is it's not set yet
    AIS-20819

--- lib/puppet/parser/functions/add_vm_tag.rb
+++ lib/puppet/parser/functions/add_vm_tag.rb
@@ -1,7 +1,7 @@
 require File.join(File.dirname(__FILE__), '../../../../../../forge/cloudstack/lib/util/cloudstack_client')
 
 module Puppet::Parser::Functions
-  newfunction(:add_vm_tag, :type => :rvalue) do |args|    
+  newfunction(:add_vm_tag, :type => :rvalue) do |args|
 
     virtual_machine_name = args[0]
     key                  = args[1]
@@ -9,29 +9,37 @@ module Puppet::Parser::Functions
 
     if virtual_machine_name.empty?() then
       fail('add_vm_tag requires a virtual machine name')
-    end    
+    end
 
     if key.empty?() then
       fail('add_vm_tag requires a tag key')
-    end        
+    end
 
     if value.empty?() then
       fail('add_vm_tag requires a tag value')
     end
 
-    cc = Object.new  
-    cc.extend(CloudstackClient::Helper)       
+    cc = Object.new
+    cc.extend(CloudstackClient::Helper)
 
     project = cc.get_project
-    project_id = project['id']    
-    
+    project_id = project['id']
+
     vm = cc.api.get_server(virtual_machine_name, project_id)
-    if vm then
-      cc.api.add_tag_for_resource(project_id, 'UserVM', vm['id'], key, value)          
+    tags = cc.api.get_tags_for_resource(project_id, 'UserVM', vm['id'])
+
+    if tags[0]['key'] == key and tags[0]['value'] == value
+      # Do nothing, tag already exists
+      true
     else
-      err "Could not get data for VM from Cloudstack API."
-      false
+      if vm then
+        puts "Add Tag!"
+        cc.api.add_tag_for_resource(project_id, 'UserVM', vm['id'], key, value)
+      else
+        err "Could not get data for VM from Cloudstack API."
+        false
+      end
+      true
     end
-    true
   end
 end
